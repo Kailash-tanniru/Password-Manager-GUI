e3af969aa68b44bfca0479f3d7600cef58563664
kailash <tannirukailash@gmail.com>
2022-01-13 22:54:54 +0530

    Added some files in backend.py

--- Password Manager/backend.py
+++ Password Manager/backend.py
@@ -1,9 +1,23 @@
 from tkinter import *
 import sqlite3
+from tkinter import messagebox
 
 class DB:
-    def __init__(self):
-        pass
+    def create_db(self):
+        self.con   = sqlite3.connect('GIT/Password-Manager-GUI/Password Manager/app.db')
+        self.cur = self.con.cursor()
+        self.cur.execute("CREATE TABLE IF NOT EXISTS userdcred (username text,profession text,age int,gender text,password text,)")
+        self.con.commit()
+        self.con.close()
 
-    def create_database(self):
-        pass
+    def insert_into_db(self,x,y,z,g,p):
+        try:
+            self.con = sqlite3.connect("GIT/Password-Manager-GUI/Password Manager/app.db")
+            self.cur  = self.con.cursor()
+            sql_query = 'INSERT INTO usercred (username,profession,age,gender,password) VALUES(?,?);'
+            self.cur.execute(sql_query,(x,y,z,g,p))
+            self.con.commit()
+            self.con.close()
+        except Exception as es:
+            messagebox.showerror("Error,Error")
+
